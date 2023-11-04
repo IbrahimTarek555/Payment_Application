e13109c6010e185e260cd944d47e4117e9e3d124
Ibrahim Tarek <ebrahimtarek555@yahoo.com>
2023-11-04 15:40:36 +0200

    Add standard library.

---
+++ Libraries/STD_TYPES.h
@@ -0,0 +1,27 @@
+/**
+ * @file STD_TYPES.h
+ * @author Ibrahim Tarek
+ * @brief This file includes the main standard data types that will used in the project.
+ * @version 1.00
+ * @date 2023-10-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef	STD_TYPES_H_
+#define	STD_TYPES_H_
+
+typedef unsigned char	uint8_t;
+typedef signed 	 char 	sint8_t;
+
+typedef unsigned short int 	uint16_t;
+typedef signed 	 short int 	sint16_t;
+typedef unsigned long  int 	uint32_t;
+typedef signed 	 long  int 	sint32_t;
+
+typedef		 float  f32_t;
+typedef 	 double	f64_t;
+typedef long double f96_t;
+
+#endif
